98393d367aa891b9459149241009c46b609db4c8
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2023-12-25 01:17:20 +0530

    dev and prod env setup

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
 import express from "express";
-import userRoute from "./routes/user-routes.js";
+import routes from "./routes";
 import dotenv from "dotenv";
+
 dotenv.config();
 const app = express();
-const port = process.env.PORT;
 
-import ref from "./database/firebase";
-const gayMan = ref.child("gayman");
+const port = process.env.PORT || 3000;
+
+const nodeEnv = process.env.NODE_ENV;
+if (nodeEnv === "production") {
+    console.log("Running in Production Mode");
+} else console.log("Running in Development Mode");
 
 app.use(express.json());
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
-app.get("/gay", (req, res) => {
-    res.json({ gayestManAlive: gayMan });
-});
-
-app.use("/users", userRoute);
-
+app.use("/", routes);
 app.get("/", (req, res) => {
-    const nodeEnv = process.env.NODE_ENV;
-    if (nodeEnv === "production") {
-        console.log("running in production mode");
-    }
-    console.log("hi");
-    res.send("v3 1254 Hello from Homepage.");
+    res.status(200).send("Hello from the TECHRACE Homepage.");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
 });
