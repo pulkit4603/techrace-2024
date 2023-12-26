4ce23663956673dcf085586ff25e546e48f3b393
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2023-12-27 00:37:48 +0530

    model functions created

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,17 @@
 import { fsAddNewTeam, fsGetTeamData } from "./firestoreDBModels/team-models";
+import {
+    rtGetClueID,
+    rtGetRoute,
+    rtGetTeamData,
+    rtUpdateRoute,
+    rtUpdateTeamData,
+} from "./realtimeDBModels/team-models";
 
 export { fsAddNewTeam, fsGetTeamData };
+export {
+    rtGetClueID,
+    rtGetRoute,
+    rtGetTeamData,
+    rtUpdateRoute,
+    rtUpdateTeamData,
+};
---
+++ src/models/realtimeDBModels/team-models.js
@@ -0,0 +1,7 @@
+import { realtimeDB } from "../../database";
+
+export const rtGetClueID = async (clueIndex, teamID) => {};
+export const rtGetTeamData = async (teamID) => {};
+export const rtUpdateTeamData = async (teamID, payload) => {};
+export const rtGetRoute = async (teamID) => {};
+export const rtUpdateRoute = async (teamID, payload) => {};
