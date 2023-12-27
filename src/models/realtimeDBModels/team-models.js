83b3f7c9526bb94d27265dd662a1fd70a9f54e9e
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2023-12-27 11:43:53 +0530

    documented code

--- src/models/realtimeDBModels/team-models.js
+++ src/models/realtimeDBModels/team-models.js
@@ -1,6 +1,12 @@
 import { realtimeTeamDB } from "../../database";
 import { fsGetClueData } from "../firestoreDBModels";
 
+/**
+ * Takes in a clueIndex (like c1)
+ * and a teamID (like 001)
+ * and returns the clue data for that index
+ * @returns {Promise} promise that resolves with the clue data
+ */
 export const rtGetClueData = async (clueIndex, teamID) => {
     try {
         const snapShot = await realtimeTeamDB.child(teamID).once("value");
@@ -14,6 +20,10 @@ export const rtGetClueData = async (clueIndex, teamID) => {
     }
 };
 
+/**
+ * Takes in a teamID (like 001) and returns the team data {object}
+ * @returns {Promise} promise that resolves with the team data {object}
+ */
 export const rtGetTeamData = async (teamID) => {
     try {
         const snapShot = await realtimeTeamDB.child(teamID).once("value");
@@ -26,6 +36,11 @@ export const rtGetTeamData = async (teamID) => {
     }
 };
 
+/**
+ * Takes in a teamID (like 001) and a payload
+ * and updates the team data with the payload
+ * @returns {Promise} promise that resolves when the update is complete
+ */
 export const rtUpdateTeamData = async (teamID, payload) => {
     return new Promise((resolve, reject) => {
         realtimeTeamDB.child(teamID).update(payload, (error) => {
@@ -38,6 +53,10 @@ export const rtUpdateTeamData = async (teamID, payload) => {
     });
 };
 
+/**
+ * Takes in a teamID (like 001) and returns the route {object}
+ * @returns {Promise} promise that resolves with the route {object}
+ */
 export const rtGetRoute = async (teamID) => {
     try {
         const snapShot = await realtimeTeamDB.child(teamID).once("value");
@@ -50,9 +69,14 @@ export const rtGetRoute = async (teamID) => {
     }
 };
 
-export const rtUpdateRoute = async (teamID, payload) => {
+/**
+ * Takes in a teamID (like 001) and a route payload (like {route: ["c1", "c2", "c3"]})
+ * and updates the team data with the route payload
+ * @returns {Promise} promise that resolves when the update is complete
+ */
+export const rtUpdateRoute = async (teamID, routePayload) => {
     return new Promise((resolve, reject) => {
-        realtimeTeamDB.child(teamID).update(payload, (error) => {
+        realtimeTeamDB.child(teamID).update(routePayload, (error) => {
             if (error) {
                 reject(error);
             } else {
