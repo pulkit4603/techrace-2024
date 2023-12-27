1946c3e17d53677a60960b08bfca98d72bdb62cc
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2023-12-27 18:31:57 +0530

    formatted

--- src/routes/game-routes.js
+++ src/routes/game-routes.js
@@ -9,7 +9,7 @@ router.get("/", (req, res) => {
 });
 
 router.post("/powerUp", powerUp);
-router.post("/nextClue", nextClue); 
-router.post("/gethint",  gethint); //lesgo it works
+router.post("/nextClue", nextClue);
+router.post("/gethint", gethint);
 
 export default router;
