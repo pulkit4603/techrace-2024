306d23f7dc5b4cdcbd23f5813d36e01953d41b0b
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2023-12-27 11:52:55 +0530

    indexed controller exports

---
+++ src/controllers/index.js
@@ -0,0 +1,12 @@
+import { powerUp, calculatePointsToAdd, nextClue } from "./game-controllers.js";
+import { newUser, getUser } from "./user-controllers.js";
+import { login } from "./login-controllers.js";
+
+// game controllers:
+export { powerUp, calculatePointsToAdd, nextClue };
+
+// user controllers:
+export { newUser, getUser };
+
+// login controllers:
+export { login };
