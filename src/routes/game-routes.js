16b4a979abf00babc82672da4e043c918c9986d1
pulkit-gpt <hiiampulkitg@gmail.com>
2023-12-27 15:12:51 +0530

    added auth

--- src/routes/game-routes.js
+++ src/routes/game-routes.js
@@ -5,16 +5,16 @@ import{
 } from "../controllers/game-controller.js";
 
 import express from "express";
-
+import auth from "../middleware/auth.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
     res.status(200).send("Hello from Game Routes.");
 });
 
-router.post("/powerUp/:pid", powerUp);
-router.post("/nextClue", nextClue);
-router.post("/gethint", gethint);
+router.post("/powerUp/:pid",auth, powerUp);
+router.post("/nextClue", auth, nextClue);
+router.post("/gethint", auth, gethint);
 
 
 export default router;
