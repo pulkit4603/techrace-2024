92f17c688dadbf2fee703eacb97f3c9db89f13b2
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2023-12-28 21:27:41 +0530

    updated fsModels accordingly

--- src/models/firestoreDBModels/index.js
+++ src/models/firestoreDBModels/index.js
@@ -1,5 +1,12 @@
 import { fsAddData } from "./common-models";
-import { fsAddNewTeam, fsGetTeamData } from "./team-models";
+import { fsAddNewTeam, fsGetTeamData, fsUpdateTeamData } from "./team-models";
 import { fsGetClueData, fsAddClueData } from "./clue-models";
 
-export { fsAddData, fsAddNewTeam, fsGetTeamData, fsGetClueData, fsAddClueData };
+export {
+    fsAddData,
+    fsAddNewTeam,
+    fsGetTeamData,
+    fsGetClueData,
+    fsAddClueData,
+    fsUpdateTeamData,
+};
