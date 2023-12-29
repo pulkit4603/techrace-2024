bc188d8ce857311881a8185cfb88177ec87699f2
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2023-12-30 03:12:38 +0530

    created middleware factory func for validation

---
+++ src/middleware/schema-validation.js
@@ -0,0 +1,8 @@
+export const validateSchema = (schema) => async (req, res, next) => {
+    try {
+        await schema.validate(req.body);
+        next();
+    } catch (error) {
+        res.json({ status: "-1", message: "Bad Request" });
+    }
+};
