f5dd3e2cbf80d28055b4f8e516033d3015faf975
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2023-12-31 02:05:37 +0530

    added refresh route

--- src/routes/login-routes.js
+++ src/routes/login-routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { validateRequest } from "../middleware";
 import { loginSchema } from "../schemas";
-import { login } from "../controllers";
+import { login, refresh } from "../controllers";
 
 const router = express.Router();
 
 router.post("/", validateRequest(loginSchema), login);
+
+router.post("/refresh", refresh);
 
 router.get("/", (req, res) => {
     res.status(200).send("Hello from Login Routes.");
