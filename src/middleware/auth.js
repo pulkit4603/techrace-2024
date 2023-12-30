8d6309db5b0758d18e5e7bd85be4fc209aca7944
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2023-12-31 02:05:06 +0530

    added auth midware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,20 @@
-export const auth = (req, res, next) => {
-    console.log("Auth middleware");
-    next();
-    return;
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        req.teamID = decoded ? decoded.teamID : null;
+
+        next();
+    } catch (error) {
+        res.json({
+            status: "4",
+            message: "Unauthorized",
+        });
+    }
 };
