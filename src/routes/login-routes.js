c99d8c641397529ebf208fff815dc512ed33910d
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2023-12-31 14:33:15 +0530

    added refresh schema

--- src/routes/login-routes.js
+++ src/routes/login-routes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { validateRequest } from "../middleware";
-import { loginSchema } from "../schemas";
+import { loginSchema, refreshSchema } from "../schemas";
 import { login, refresh } from "../controllers";
 
 const router = express.Router();
 
 router.post("/", validateRequest(loginSchema), login);
 
-router.post("/refresh", refresh);
+router.post("/refresh", validateRequest(refreshSchema), refresh);
 
 router.get("/", (req, res) => {
     res.status(200).send("Hello from Login Routes.");
