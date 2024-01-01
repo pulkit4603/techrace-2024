c825b593d8ed44da9387d1314263557b7b23a1bb
pulkit-gpt <hiiampulkitg@gmail.com>
2024-01-02 00:24:19 +0530

    modified format

--- src/logger/winston.js
+++ src/logger/winston.js
@@ -4,7 +4,7 @@ export const logger = winston.createLogger({
     level: 'http',
     format: winston.format.combine(
         winston.format.timestamp(),
-        winston.format.printf(info => `TIME:${info.timestamp}\n${JSON.stringify(info.message)}`)
+        winston.format.printf(info => `{TIME:${info.timestamp} -- ${JSON.stringify(info.message)}}`)
     ),
     transports: [
         new winston.transports.File({ filename: 'logfile.log' }),
