ff98110141b96c3f36bf04345ecaf9639479b362
pulkit-gpt <hiiampulkitg@gmail.com>
2024-01-02 00:04:07 +0530

    added format to logger

--- src/logger/winston.js
+++ src/logger/winston.js
@@ -1,14 +1,14 @@
-const { min } = require('moment');
 const winston = require('winston');
-
-
 
 export const logger = winston.createLogger({
     level: 'http',
-    format: winston.format.json(),
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(info => `${info.timestamp} ${info.level}: ${JSON.stringify(info.message)}`)
+    ),
     transports: [
         new winston.transports.File({ filename: 'logfile.log' }),
-        new winston.transports.File({filename: 'error.log'})
+        new winston.transports.File({filename: 'error.log' , level: 'error'})
     ],
     exitOnError: false
 });
