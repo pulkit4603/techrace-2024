27fee7284fae6e5e6b20ae1ec797020d7730c99a
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2024-01-06 17:21:36 +0530

    formatted

--- src/logger/winston.js
+++ src/logger/winston.js
@@ -1,17 +1,17 @@
-const winston = require('winston');
+const winston = require("winston");
 
 export const logger = winston.createLogger({
-    level: 'http',
+    level: "http",
     format: winston.format.combine(
         winston.format.timestamp(),
-        winston.format.printf(info => `{TIME:${info.timestamp} -- ${JSON.stringify(info.message)}}`)
+        winston.format.printf(
+            (info) =>
+                `{TIME:${info.timestamp} -- ${JSON.stringify(info.message)}}`,
+        ),
     ),
     transports: [
-        new winston.transports.File({ filename: 'logfile.log' }),
-        new winston.transports.File({filename: 'error.log' , level: 'error'})
+        new winston.transports.File({ filename: "logfile.log" }),
+        new winston.transports.File({ filename: "error.log", level: "error" }),
     ],
-    exitOnError: false
+    exitOnError: false,
 });
-
-
-
