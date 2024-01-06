5974392b32fcf18da537b0895c71181999fe4bd1
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2024-01-06 11:54:57 +0530

    hardcoded values shifted to config

---
+++ src/config/game-config.js
@@ -0,0 +1,16 @@
+export default {
+    freezeTime: 10 * 60, //10 minutes
+    freezeCooldownDuration: 15 * 60, //15 minutes
+    invisibleTime: 10 * 60, //10 minutes
+    meterOffTime: 15 * 60, //15 minutes
+    numberOfRoutes: 2, //confirm with naman
+    maxTeamID: 200, // for mutex
+
+    costFreezeTeam: 125,
+    costMeterOff: 100,
+    costInvisible: 130,
+    costReverseFreeze: 175,
+    costSkipLocation: 225,
+    costAddLocation: 200,
+    costMysteryCard: 250,
+};
