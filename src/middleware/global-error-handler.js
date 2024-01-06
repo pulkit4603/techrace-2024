dc2ef60e1b0d00f0c3a20924131112f71f81f32f
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2024-01-06 22:58:04 +0530

    logged in error handler

--- src/middleware/global-error-handler.js
+++ src/middleware/global-error-handler.js
@@ -1,18 +1,17 @@
 import { CustomError } from "../errors/custom.error";
-import { logger } from "../utils/logger";
+import { logger } from "../logger/winston.js";
 import { StatusCodes } from "http-status-codes";
 
-export const globalErrorHandler = (err, req, res) => {
-    logger.error({
-        level: "error",
-        message: `Error Ocurred on route ${req.path}: ${err.message}`,
-    });
-
+export const globalErrorHandler = (err, req, res, _next) => {
     if (err instanceof CustomError) {
         res.status(err.statusCode).json({
             status: "Failure",
             message: err.message,
-            data: err,
+        });
+
+        logger.error({
+            message: `Error Ocurred on route ${req.path}: ${err.message}`,
+            meta: err.data,
         });
         return;
     }
@@ -21,5 +20,9 @@ export const globalErrorHandler = (err, req, res) => {
         status: "Failure",
         message: err.message ? err.message : "Something went wrong",
         data: err,
+    });
+    logger.error({
+        level: "error",
+        message: `Internal server error on ${req.path}: ${err.message}`,
     });
 };
